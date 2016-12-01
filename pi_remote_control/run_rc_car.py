fdb766395f865073ef071ce161e8e0a8ebbf8d63
Henry Fyfe <henryfyfe@gmail.com>
2016-11-30 20:41:24 -0600

    reorganizing repo

--- pi_remote_control/run_rc_car.py
+++ pi_remote_control/run_rc_car.py
@@ -4,25 +4,24 @@ from time import sleep
 class PiCar(object):
 
 	def __init__(self):
-		GPIO.setmode(GPIO.BOARD)
+        	GPIO.setmode(GPIO.BOARD)
         	self.pins = {'left'     : 18,
                 	     'right'    : 11,
                 	     'forward'  : 16,
-                     	     'backward' : 12 }
+                     	 'backward' : 12 }
         	for pin_number in self.pins.itervalues():
             		GPIO.setup(pin_number,GPIO.OUT)
-		self.stop()
-
+        	self.stop()
 
 	def stop(self):
-        	for pin_number in self.pins.itervalues():
-             		GPIO.output(pin_number,0)
+    	   for pin_number in self.pins.itervalues():
+         		GPIO.output(pin_number,0)
 
-     	def go_forward(self,time):
-        	self.stop()
-         	GPIO.output(self.pins['forward'],1)
-         	sleep(float(time))
-         	self.stop()
+ 	def go_forward(self,time):
+    	   self.stop()
+     	   GPIO.output(self.pins['forward'],1)
+     	   sleep(float(time))
+     	   stopelf.stop()
 
 	def go_backward(self,time):
         	self.stop()
@@ -30,7 +29,7 @@ class PiCar(object):
          	sleep(float(time))
          	self.stop()
 
-     	def go_forward_right(self,time):
+    def go_forward_right(self,time):
         	self.stop()
          	GPIO.output(self.pins['right'],1)
          	GPIO.output(self.pins['forward'],1)
@@ -38,19 +37,19 @@ class PiCar(object):
          	self.stop()
 
     def turn_wheels_left(self, time):
-        self.stop()
-        GPIO.output(self.pins['left'],1)
-        sleep(float(time))
-        self.stop()
+            self.stop()
+            GPIO.output(self.pins['left'],1)
+            sleep(float(time))
+            self.stop()
 
     def turn_wheels_right(self, time):
-        self.stop()
-        GPIO.output(self.pins['right'],1)
-        sleep(float(time))
-        self.stop()
+            self.stop()
+            GPIO.output(self.pins['right'],1)
+            sleep(float(time))
+            self.stop()
 
 	def go_forward_left(self,time):
-		self.stop()
+		    self.stop()
          	GPIO.output(self.pins['left'],1)
          	GPIO.output(self.pins['forward'],1)
          	sleep(float(time))
